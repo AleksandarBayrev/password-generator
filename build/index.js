aa5b7e4d223840cbdcca0dbe3f77fc22d3afffd6
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2021-07-06 01:48:28 +0300

    Remove build folder

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var getLengthArgument_1 = require("./getLengthArgument");
-var instances_1 = require("./instances");
-var length = getLengthArgument_1.getLengthArgument(process.argv);
-console.log("Your password with length: " + length + " is \n");
-console.log(instances_1.passwordService.generate(length));
