47bf7fbe9c35ebda5b450caf26c5cd94de358c5e
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2021-07-06 12:36:47 +0300

    Update jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverageFrom: [
+    "**/*.{ts,tsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**"
+  ],
+  testMatch: [
+    "**/?(*.)+(spec|test).[t]s?(x)"
+  ]
 };
